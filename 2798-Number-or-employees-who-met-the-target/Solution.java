21fe8385dadc80925ee46f3f9ad0600283dd6e31
ROHIT <msjcbse20210939@gmail.com>
2026-07-13 10:14:08 +0530

    employee meet the target

---
+++ 2798-Number-or-employees-who-met-the-target/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        for(int i=0;i<=hours.length-1;i++){
+            if(hours[i]>=target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
