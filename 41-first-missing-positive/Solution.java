61f6a352b58e95fa9ba8a0c114ee41915c0f50f7
ROHIT <msjcbse20210939@gmail.com>
2026-07-15 13:52:29 +0530

    missing positive

---
+++ 41-first-missing-positive/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=1;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=0){
+                continue;
+            }
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            if(nums[i]!=n){ 
+            return n;
+            }
+            n++;
+        }
+        return n;
+    }
+}
