0b0555af513831247d3c3832f43d76111fb7c946
ROHIT <msjcbse20210939@gmail.com>
2026-07-30 22:31:15 +0530

    three consecutive odd

---
+++ 1550-three-consecutive-odd/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count=0;
+        for(int i:arr){
+            if(i%2==1){
+                count++;
+            }
+            if(i%2==0){
+                count=0;
+            }
+            if(count==3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
