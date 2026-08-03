253368e486783e885e912c14d978d6b9f6c352fc
ROHIT <msjcbse20210939@gmail.com>
2026-08-03 21:59:36 +0530

    max unit of truck

---
+++ 1710-maximum-unit-on-truck/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        // Arrays.sort(boxTypes);
+        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+
+        int ans=0;
+     for(int[] box:boxTypes){
+        if(truckSize>=box[0]){
+            ans+=box[0]*box[1];
+            truckSize-=box[0];
+        }
+        else{
+        ans+=truckSize*box[1];
+        truckSize=0;
+        break;
+        }
+     }
+        return ans;
+    }
+}
