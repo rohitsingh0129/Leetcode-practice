7d5a79225a1d7356c7ceb53c7474aabdb79e56b8
ROHIT <msjcbse20210939@gmail.com>
2026-08-08 23:22:00 +0530

    arranging coin

---
+++ 441-arranging-coins/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+       int ans=0;
+       int index=1;
+        while(n>=index){
+            ans++;
+            n-=index;
+            index++;
+        }
+        return ans;
+    }
+}
